3ed09878c2314b5dd3d281ebe31172fbbb35f6c8
Kamil Gieras <104592814+kamilGie@users.noreply.github.com>
2024-07-11 00:12:54 +0200

    one depth chess ai

--- src/ChessAI/ChessAI.cpp
+++ src/ChessAI/ChessAI.cpp
@@ -11,14 +11,25 @@ ChessAI::~ChessAI() {
 }
 
 Move* ChessAI::GetMove(Chessboard& chessboard) {
+    struct BestMove {
+        float score=-1;
+        Vector2 from;
+        Vector2 to;
+    };
+    BestMove bestMove;
     for (int i = 0; i < 8; i++) {
         for (int j = 0; j < 8; j++) {
             if (chessboard.grid[i][j] && chessboard.grid[i][j]->color == colorAI) {
-                for (auto& move : chessboard.grid[i][j]->legalMoves) {
-                        return new Move({(float)i, (float)j}, move,chessboard);
+                for (auto& [x,y] : chessboard.grid[i][j]->legalMoves) {
+
+                        if(bestMove.score==-1 || (chessboard.grid[(int)x][(int)y] && chessboard.grid[(int)x][(int)y]->color!=colorAI && bestMove.score < chessboard.grid[(int)x][(int)y]->getValue())){
+                            bestMove.score= chessboard.grid[int(x)][int(y)] ? chessboard.grid[int(x)][int(y)]->getValue() : 0; 
+                            bestMove.from={(float)i,(float)j};
+                            bestMove.to={x,y};
+                        }
                 }
             }
         }
     }
-    return nullptr;
+    return new Move(bestMove.from,bestMove.to,chessboard);
 }
