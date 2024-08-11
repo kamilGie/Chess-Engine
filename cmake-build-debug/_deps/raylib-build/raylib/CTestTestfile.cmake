993c4c9f95de825a399b5560e513ee74a6b169d6
Kamil Gieras <104592814+kamilGie@users.noreply.github.com>
2024-08-11 17:48:50 +0200

    delete debug

--- cmake-build-debug/_deps/raylib-build/raylib/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/koxu2115/Desktop/kody/chess/cmake-build-debug/_deps/raylib-src/src
-# Build directory: /Users/koxu2115/Desktop/kody/chess/cmake-build-debug/_deps/raylib-build/raylib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("external/glfw")
